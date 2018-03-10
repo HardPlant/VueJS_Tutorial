968e01532d07f3694e64af5a8f73e7033d7d202a
HardPlant <abc7988se@naver.com>
2018-03-10 11:41:26 +0900

    webpack_server

--- Chapter 8/webpactest/webpack.config.js
+++ Chapter 8/webpactest/webpack.config.js
@@ -1,10 +1,12 @@
 var webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
 module.exports = {
     entry: {
         main: __dirname + '/src/index.js',
     },
     output: {
-        path: __dirnames + '/public/dist/',
+        path: __dirname + '/public/dist/',
         filename: '[name].js',
         publicPath: '/dist'
 
@@ -13,10 +15,27 @@ module.exports = {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
-            exclude: /node_moudles/
+            exclude: /node_modules/
         }]
     },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ]
+    plugins: [],
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: false,
+                    emca: 6,
+                    mangle: true
+                },
+                sourceMap: true
+            }),
+        ],
+    },
+    devServer: {
+        contentBase: './public',
+        port: 3000,
+        histroyApiFallback: true
+    }
 }
