11161d626a88a31f30627df3f7c9f014a9e99cce
HardPlant <abc7988se@naver.com>
2018-03-10 12:31:17 +0900

    it works

--- Chapter 9/todolistapp/src/components/InputTodo.vue
+++ Chapter 9/todolistapp/src/components/InputTodo.vue
@@ -28,12 +28,13 @@
     <input class='input' type='text' id='task' v-model.trim='todo'
     placeholder="Enter!" v-on:keyup.enter="addTodo">
     <span class="addbutton" v-on:click="addTodo">추 가</span>
+</div>
 </template>
 
 <script type="text/javascript">
 import eventBus from './EventBus.vue'
 
-export default{
+export default {
     name: 'input-todo',
     data: function() {
         return {
