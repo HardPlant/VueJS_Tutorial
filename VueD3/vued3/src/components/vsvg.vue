ce0b873876c0f246f7689a23d2e90ee0b22de2f0
HardPlant <abc7988se@naver.com>
2018-04-04 21:25:16 +0900

    draw a line

--- VueD3/vued3/src/components/vsvg.vue
+++ VueD3/vued3/src/components/vsvg.vue
@@ -4,6 +4,7 @@
         <line x1= "100" y1 = "100" x2="00" y2="200"
         style = "stroke:rgb(255,0,0);stroke-width:2"/>
     </svg>
+    <div id = "svgcontainer"></div>
 </div>
 
 </template>
@@ -15,7 +16,18 @@ export default {
         return {};
     },
     mounted : function(){
-
+        var d3 = this.$d3;
+        var width = 300;
+        var height = 300;
+        var svg = d3.select("#svgcontainer")
+        .append("svg").attr("width",width).attr("height", height);
+        svg.append("line")
+        .attr("x1", 100)
+        .attr("y1", 100)
+        .attr("x2", 200) 
+        .attr("y2", 200)
+        .style("stroke", "rgb(255,0,0)")
+        .style("stroke-width", 2);
     }
 }
 </script>
