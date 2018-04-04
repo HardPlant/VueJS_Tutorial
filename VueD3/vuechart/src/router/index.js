db2b603b36c40c2eeef055f7489b14ddf4c0cee1
HardPlant <abc7988se@naver.com>
2018-04-04 23:33:27 +0900

    charts

--- VueD3/vuechart/src/router/index.js
+++ VueD3/vuechart/src/router/index.js
@@ -2,14 +2,32 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import VueChartJS from '@/components/VueChartJS'
+import VueChartKick from '@/components/VueChartKick'
+import VueCharts from '@/components/VueCharts'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/chartjs',
+            name: 'VueChartJS',
+            component: VueChartJS
+        },
+        {
+            path: '/chartkick',
+            name: 'VueChartKick',
+            component: VueChartKick
+        },
+        {
+            path: '/charts',
+            name: 'VueCharts',
+            component: VueCharts
+        }
+    ]
 })
