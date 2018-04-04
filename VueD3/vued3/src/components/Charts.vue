059c67c5f72ca58e3c91e68054988c4f78615405
HardPlant <abc7988se@naver.com>
2018-04-04 22:58:27 +0900

    dependancy

--- VueD3/vued3/src/components/Charts.vue
+++ VueD3/vued3/src/components/Charts.vue
@@ -2,6 +2,7 @@
 <div>
 <button @click="bar"></button>
 <button @click="circle"></button>
+<button @click="pie"></button>
 </div>
 </template>
 
@@ -85,33 +86,63 @@ export default {
         },
         pie : function(){
             var d3 = this.$d3;
-            var svg = d3.select("svg"),
-                width = svg.attr("width"),
-                height = svg.attr("height"),
-                radius = Math.min(width, height) / 2;
-            var g = svg.append("g")
-                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-            var color = d3.scaleOrdinal(['gray', 'green', 'brown', 'orange']);
-            var pie = d3.pie()
-                .value(function(d) { return d.percent; });
-            var arc = d3.arc()
-                .outerRadius(radius)
-                .innerRadius(0);
-            var label = d3
-                .arc()
-                .outerRadius(radius)
-                .innerRadius(radius - 80);
-            d3.csv("populations.csv", function(error, data) {
-                if (error) {
-                    throw error;
-                }
-            });
+
+            var width = 400;
+            var height = 400;
+            
+            var svg = d3.select("body")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height);
+
+            var radius = Math.min(width, height) / 2;
+        console.log(svg)
+         var g = svg.append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+         var color = d3.scaleOrdinal([
+            'gray', 'green', 'brown', 'orange', 'yellow', 'red', 'purple'
+         ]);
+         
+         var pie = d3.pie().value(function(d) { 
+            return d.percent; 
+         });
+         
+         var path = d3.arc()
+            .outerRadius(radius - 10).innerRadius(0);
+        
+         var label = d3.arc()
+            .outerRadius(radius).innerRadius(radius - 80);
+         
+         d3.csv("populations.csv", function(error, data) {
+            if (error) {
+               throw error;
+            }
+            
             var arc = g.selectAll(".arc")
-                .data(pie(data))
-                .enter()
-                .append("g")
-                .attr("class", "arc");
-                        }
+               .data(pie(data))
+               .enter()
+               .append("g")
+               .attr("class", "arc");
+            
+            arc.append("path")
+               .attr("d", path)
+               .attr("fill", function(d) { return color(d.data.states); });
+        
+            console.log(arc)
+        
+            arc.append("text").attr("transform", function(d) { 
+               return "translate(" + label.centroid(d) + ")"; 
+            })
+            
+            .text(function(d) { return d.data.states; });
+         });
+         
+         svg.append("g")
+            .attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
+            .append("text").text("Top population states in india")
+            .attr("class", "title")
+            }
     }
 }
 </script>
