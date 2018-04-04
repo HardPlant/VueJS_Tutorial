c053c4cb9f130c268e252d14634bc44d2b2fd1fa
HardPlant <abc7988se@naver.com>
2018-04-04 20:45:22 +0900

    classed

--- VueD3/vued3/src/components/HelloWorld.vue
+++ VueD3/vued3/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-  <div id="hello">
-    Hello World
+  <div class="myclass">
+      Hello World!
   </div>
 </div>
 </template>
@@ -15,8 +15,14 @@ export default {
     }
   },
   mounted: function(){
+    var d3 = this.$d3;
     //this.$d3.select("#hello").append("span");
-    this.$d3.select("#hello").append("span").text("from D3.js");
+    //this.$d3.select("#hello").append("span").text("from D3.js");
+    //this.$d3.select("#hello").html("Hello World! from vue!");
+    //this.$d3.select(".myclass").attr("style", "color: red");
+    //d3.select(".myclass").style("color", "red");
+    //var element = d3.select(".myclass")
+    //element.classed("myanotherclass", !oneBar.classed("myanotherclass"));
   }
 }
 </script>
