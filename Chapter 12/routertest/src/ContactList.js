77fc63fe3ac3cfd1d2fb4f2952ccfdae5bbfd499
HardPlant <abc7988se@naver.com>
2018-04-12 16:02:15 +0900

    now handle contact

---
+++ Chapter 12/routertest/src/ContactList.js
@@ -0,0 +1,19 @@
+var contactlist = {
+    contacts: [
+        { no: 1001, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1002, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1003, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1004, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1005, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1006, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1007, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1008, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1009, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1010, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1011, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1012, name: '김유신', tel: '010-1212-3331', address: '경주' },
+        { no: 1013, name: '김유신', tel: '010-1212-3331', address: '경주' },
+    ]
+}
+
+export default contactlist;
